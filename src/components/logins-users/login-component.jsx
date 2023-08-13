cd901b7ea74e12354baa761b9dc5b7f3d10f0226
Kaif Arslan <kaifarslan1@gmail.com>
2023-08-13 19:55:57 +0530

    basic frame

--- src/components/logins-users/login-component.jsx
+++ src/components/logins-users/login-component.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <h1> <br /><br /><br />hello world</h1>
+    <div className='login'>
+    <br /><br /><br />
+    <h1>hello world</h1>
+    <h1>email</h1>
+    <input type='text' class='login-text' />
+    <h1>Password</h1>
+    <input type="text" name="" id="" />
+    <button>Sign In</button>
+    </div>
   )
+
 }
 
 export default Login
