8b85c15f6e054965b526420246148d74e044f83e
Kaif Arslan <kaifarslan1@gmail.com>
2023-08-13 19:26:18 +0530

    login route created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './style.css';
-import Navbar from './components/navbar.js';
-import Card from './components/card.js';
-import Product from './components/Product.js';
+import Navbar from './components/navbar.jsx';
+import Card from './components/card.jsx';
+import Product from './components/Product.jsx';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { useEffect ,useState} from 'react';
-import Cart from './components/Cart.js'
+import Cart from './components/Cart.jsx'
 import {createContext} from 'react'
-import About from './components/About.js'
-import Footer from './components/footer.js'
-
+import About from './components/About.jsx'
+import Footer from './components/footer.jsx'
+import Login from './components/logins-users/login-component.jsx'
 
 export const cartContext = createContext('light');
 
@@ -40,6 +40,7 @@ export default function App() {
        <Route path='/product/:productId' element={ <Product content={data} />} />
        <Route path='/cart' element={<Cart content={data} />} />     
        <Route path='/about' element={<About />} />
+       <Route path='/login' element={<Login />} />
       </Routes>
       </cartContext.Provider>
       <Footer />
---
+++ src/components/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function About(){
+
+ return ( 
+   <div className='about-page'>
+   <div className='about-section'>
+     <h1>About Me</h1>
+     <p>Hey there! I'm Kaif Arslan</p>
+     <p>A Frontend Developer with expertise in React, JavaScript, HTML & CSS. I also have experience in Python and C programming. I've honed my skills in crafting responsive web designs that deliver exceptional user experiences. Constantly learning and staying updated with the latest trends, I thrived in collaborative environments and believed effective communication was key to success. Let's create something incredible together!
+</p>
+     <p>
+      As you might already came to know that this is a project to show my frontend skills not the a real shopping site. This project uses the following technologies
+     </p>
+     <ul>
+       <li>React</li>
+       <li>React Router</li>
+       <li>HTML & CSS</li>
+     </ul>
+     <p>Github link of this repository 👉 <a href='https://github.com/KaifArslan/shopping-site.git'><i class="fab fa-github aboutGit"></i></a></p>
+     <hr />
+
+   </div>
+   </div>
+ 
+ )
+}
