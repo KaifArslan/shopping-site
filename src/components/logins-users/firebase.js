c2e7435570f2e3fd8d1d762d9b8012a48e9de68e
Kaif Arslan <kaifarslan1@gmail.com>
2023-08-16 15:09:00 +0530

    some firebase inititalization

---
+++ src/components/logins-users/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsXzc4SGyLXZ1J6lrpDa-s2ymsmO6WYWM",
+  authDomain: "shopping-kaif.firebaseapp.com",
+  projectId: "shopping-kaif",
+  storageBucket: "shopping-kaif.appspot.com",
+  messagingSenderId: "875531013197",
+  appId: "1:875531013197:web:eeeb51d9d3ac995123e004"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/components/logins-users/login-component.jsx
+++ src/components/logins-users/login-component.jsx
@@ -3,13 +3,22 @@ import './login.css'
 
 const Login = () => {
   return (
+    <>
     <div className='login'>
     <h1>Email</h1>
-    <input type='email' class='login-mail' name='email' />
+    <input type='email' className='login-mail' name='email' />
     <h1>Password</h1>
     <input type="password" name="password"  />
     <button>Sign In</button>
     </div>
+    <div className='register'>
+    <h1>Email</h1>
+    <input type='email' className='register-mail' name='email' />
+    <h1>Password</h1>
+    <input type="password" className="password-register" name='password-register' />
+    <button>Sign In</button>
+    </div>
+    </>
   )
 
 }
